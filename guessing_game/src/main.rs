cdd05f642bd66edd688497e72a3bd8f6ae508294
Gabriel Soares <gabrisoares1020@hotmail.comgit config --global user.email gabrisoares1020@hotmail.com>
2022-04-02 16:16:47 -0300

    Guess the Number 1.0

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,25 @@
-use std::io; //Biblioteca
-use rand::Rng; //Números aleátorios
+use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
-fn main (){
-
-    println!("Adivinhe o Número");
-
-    let numero_secreto = rand::thread_rng().gen_range(1..101);
-
-    println!("O Número Secreto é: {}", numero_secreto); // {} segura valores como um tipo de vetor
-
-    println! ("Insira o seu palpite.");
-
-    let mut guess = String::new(); //Variável que recebe o valor
-
-    io::stdin() //Função que receberá o input (Função da biblioteca) 
-        .read_line(&mut guess) //Linha que coleta o input do usuário
-        .expect("Falha ao ler a linha"); //Linha de teste?
-
-    println! ("Você colocou: {}", guess);
+fn main(){
 
     
+    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha ao ler a linha");
+
+    let guess: u32 = guess.trim().parse().expect("Por favor digite um número");
+
+    println!("Você adivinhou: {}",guess);
+
+    match guess.cmp(&secret_number){
+
+        Ordering::Less => println!("Muito pequeno"),
+        Ordering::Greater => println! ("Muito grande"),
+        Ordering::Equal => print! ("Você ganhou"),
+    }
 }
--- guessing_game/target/debug/deps/rand_chacha-b2019e0c2ee0518d.d
+++ guessing_game/target/debug/deps/rand_chacha-b2019e0c2ee0518d.d
@@ -1,7 +1,7 @@
-/home/sonsyrock/RustLang/guessing_game/target/debug/deps/rand_chacha-b2019e0c2ee0518d.rmeta: /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+/home/sonsyrock/RustLang/guessing_game/target/debug/deps/rand_chacha-b2019e0c2ee0518d.rmeta: /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
 
-/home/sonsyrock/RustLang/guessing_game/target/debug/deps/rand_chacha-b2019e0c2ee0518d.d: /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+/home/sonsyrock/RustLang/guessing_game/target/debug/deps/rand_chacha-b2019e0c2ee0518d.d: /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
 
-/home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
+/home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
+/home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
+/home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
