23d580e3a3ad39ffdff5744967c3efec66eb72c6
Gabriel Soares <gabrisoares1020@hotmail.comgit config --global user.email gabrisoares1020@hotmail.com>
2022-04-02 17:14:47 -0300

    Adivinhe o número 1.1 - Próximo "Allowing Multiple Guesses"

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,18 +3,22 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main(){
-
     
-    let mut guess = String::new();
+    
+    println! ("Adivinhe o Número!");
     let secret_number = rand::thread_rng().gen_range(1..101);
+  
+    println! ("O número secreto é: {}", secret_number);
+    println! ("Favor digite um número:");
 
+    let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Falha ao ler a linha");
+           
+    let guess: u32 = guess.trim().parse().expect("Por favor digite um número.");
 
-    let guess: u32 = guess.trim().parse().expect("Por favor digite um número");
-
-    println!("Você adivinhou: {}",guess);
+    println! ("Você digitou: {}", guess);
 
     match guess.cmp(&secret_number){
 
