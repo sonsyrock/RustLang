6e62a575ba2ca39e4ed13678c02d8c9db83c9a77
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-05 10:58:54 -0300

    Guessing Game - Complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,7 +9,7 @@ fn main(){
 
     let secret_number = rand::thread_rng().gen_range(1..101); //Variável do número secreto que gera valores aleatórios
   
-    println! ("O número secreto é: {}", secret_number);
+    //println! ("O número secreto é: {}", secret_number); Comentado para não aparecer o número secreto
 
     loop {
 
@@ -20,7 +20,20 @@ fn main(){
             .read_line(&mut guess) //Lê a variável
             .expect("Falha ao ler a linha");
            
-    let guess: u32 = guess.trim().parse().expect("Por favor digite um número."); //verifica se foi digitado um número ou uma letra
+    //let guess: u32 = guess.trim().parse().expect("Por favor digite um número."); 
+
+
+/*
+    Assim como com o "Ordering" aqui utilizamos o match para verificar as condições, da "Ok" se for um número
+    ou dá "Err" se não for e continua o programa ao invés de exibir um erro.
+    Se o parse for bem sucedido (converter string para número) vai retornar o resultado com o número e mandar para "num" que depois armazena
+    na variável "guess".
+*/
+
+    let guess: u32 = match guess.trim().parse(){ //verifica se foi digitado um número ou uma letra
+        Ok(num) => num,
+        Err(_) => continue, // O "_" foi utilizado para ele pegar qualquer tipo de valor, não importa o tipo.
+    };
 
     println! ("Você digitou: {}", guess);
 
@@ -30,7 +43,7 @@ fn main(){
         Ordering::Greater => println! ("Muito grande"),
         Ordering::Equal => {
             
-            println! ("Você ganhou");
+            println! ("Você ganhou!");
             break;
             }
 
