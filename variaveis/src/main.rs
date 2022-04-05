1c5f1c3dfadaae74e6effcdb548972c521c10d77
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-05 14:27:31 -0300

    Fim Variáveis

--- variaveis/src/main.rs
+++ variaveis/src/main.rs
@@ -1,6 +1,18 @@
 fn main(){
-    let mut x = 5;
+    let x = 5;
+
+    let x = x +1;
+
+    {
+        let x = x * 2;
+        println!("O valor de 'x' no escopo do meio é: {}", x);
+
+    }
+
     println! ("O valor de x é: {}", x);
-    x = 6;
-    println! ("O valor de x é: {}", x);
+
+    /*const TRES_HORAS_EM_SEGUNDOS: u32 = 60*60*3;
+    println! ("3 Horas em segundos: {}", TRES_HORAS_EM_SEGUNDOS);*/
+
+
 }
