7ed04432b15c8008c479476c6a96d4f306df6d85
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-05 10:28:03 -0300

    Guessing Game 1.1 - Comentários

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,27 +1,27 @@
 use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use std::cmp::Ordering; //Faz comparação entre números
+use std::io; //Permite que o usuário insira valores a uma variável
 
 fn main(){
     
     
     println! ("Adivinhe o Número!");
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..101); //Variável do número secreto que gera valores aleatórios
   
     println! ("O número secreto é: {}", secret_number);
     println! ("Favor digite um número:");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); //guess é mutável e recebe o valor digitado pelo usuário
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut guess) //Lê a variável
             .expect("Falha ao ler a linha");
            
-    let guess: u32 = guess.trim().parse().expect("Por favor digite um número.");
+    let guess: u32 = guess.trim().parse().expect("Por favor digite um número."); //verifica se foi digitado um número ou uma letra
 
     println! ("Você digitou: {}", guess);
 
-    match guess.cmp(&secret_number){
-
+    match guess.cmp(&secret_number){ //Faz a comparação entre a variável guess e secret_number
+        
         Ordering::Less => println!("Muito pequeno"),
         Ordering::Greater => println! ("Muito grande"),
         Ordering::Equal => print! ("Você ganhou"),
--- guessing_game/target/debug/deps/cfg_if-0a04e74fc4f21d18.d
+++ guessing_game/target/debug/deps/cfg_if-0a04e74fc4f21d18.d
@@ -1,7 +1,7 @@
-/home/sonsyrock/RustLang/guessing_game/target/debug/deps/cfg_if-0a04e74fc4f21d18.rmeta: /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+/home/sonsyrock/RustLang/guessing_game/target/debug/deps/cfg_if-0a04e74fc4f21d18.rmeta: /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
 
-/home/sonsyrock/RustLang/guessing_game/target/debug/deps/libcfg_if-0a04e74fc4f21d18.rlib: /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+/home/sonsyrock/RustLang/guessing_game/target/debug/deps/libcfg_if-0a04e74fc4f21d18.rlib: /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
 
-/home/sonsyrock/RustLang/guessing_game/target/debug/deps/cfg_if-0a04e74fc4f21d18.d: /home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+/home/sonsyrock/RustLang/guessing_game/target/debug/deps/cfg_if-0a04e74fc4f21d18.d: /home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
 
-/home/sonsyrock/.asdf/installs/rust/1.59.0/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
+/home/sonsyrock/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
