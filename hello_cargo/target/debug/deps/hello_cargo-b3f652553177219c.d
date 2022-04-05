30b65095ff61738bc9a59599a72adbe8104ea792
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-05 11:57:41 -0300

    Correção Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.d
@@ -0,0 +1,5 @@
+/home/sonsyrock/RustLang/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.rmeta: src/main.rs
+
+/home/sonsyrock/RustLang/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-71d2ac1bb6e42c3d.d
@@ -0,0 +1,5 @@
+/home/sonsyrock/RustLang/hello_cargo/target/rls/debug/deps/hello_cargo-71d2ac1bb6e42c3d.rmeta: src/main.rs
+
+/home/sonsyrock/RustLang/hello_cargo/target/rls/debug/deps/hello_cargo-71d2ac1bb6e42c3d.d: src/main.rs
+
+src/main.rs:
